c505edc745057f6c28138f9449ca78fb12a9da80
user <60804817+ichg4354@users.noreply.github.com>
2021-02-02 22:23:53 +0900

    add camera

--- App.js
+++ App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Camera } from "expo-camera";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const { width: WIDTH, height: HEIGHT } = Dimensions.get("window");
+  const [premission, setPremission] = useState("noValue");
+  const getPremission = async () => {
+    try {
+      const { status } = await Camera.requestPermissionsAsync();
+      setPremission(true);
+    } catch (error) {
+      console.log(error);
+      setPremission(false);
+    }
+  };
+  useEffect(() => {
+    getPremission();
+  }, []);
+  if (premission === true) {
+    return (
+      <View style={styles.container}>
+        <Camera style={{ width: WIDTH - 30, height: HEIGHT / 1.3 }} />
+      </View>
+    );
+  } else if (premission === false) {
+    return (
+      <View style={styles.container}>
+        <Text>DENIED</Text>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator></ActivityIndicator>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
