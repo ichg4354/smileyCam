9fc23bd99af5b2e0d44a420b92778daff0195736
user <60804817+ichg4354@users.noreply.github.com>
2021-02-03 16:03:54 +0900

    finish logic for pitch zoom camera

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import {
   ActivityIndicator,
+  Animated,
   Dimensions,
   StyleSheet,
   Text,
@@ -11,6 +12,7 @@ import {
 import { Camera } from "expo-camera";
 import { Ionicons } from "@expo/vector-icons";
 import styled from "styled-components";
+import { PinchGestureHandler } from "react-native-gesture-handler";
 
 const Container = styled.View`
   flex: 1;
@@ -22,6 +24,7 @@ const Container = styled.View`
 const IconContainer = styled.View`
   flex-direction: row;
   justify-content: center;
+  margin-top: 10px;
 `;
 
 export default function App() {
@@ -30,7 +33,23 @@ export default function App() {
   const [cameraSetting, setCameraSetting] = useState({
     side: "back",
     icon: "camera-reverse",
+    zoom: 0,
   });
+
+  const zoomValue = new Animated.Value(0);
+  const handleZoomGesture = (e) => {
+    let value = e.nativeEvent.scale;
+    zoomValue.setValue(value);
+    const zoomScale = interpolateZoomValue;
+    console.log(zoomScale);
+    setCameraSetting({ zoom: interpolateZoomValue });
+  };
+  const interpolateZoomValue = zoomValue.interpolate({
+    inputRange: [0, 50],
+    outputRange: [0, 1],
+    extrapolate: "identity",
+  });
+
   const getPremission = async () => {
     try {
       const { status } = await Camera.requestPermissionsAsync();
@@ -40,6 +59,7 @@ export default function App() {
       setPremission(false);
     }
   };
+
   const handleCameraSideChangeBtnClick = () => {
     if (cameraSetting.side === "back") {
       setCameraSetting({ side: "front", icon: "camera-reverse-outline" });
@@ -47,16 +67,23 @@ export default function App() {
       setCameraSetting({ side: "back", icon: "camera-reverse" });
     }
   };
-  useEffect(() => {
+
+  useLayoutEffect(() => {
     getPremission();
   }, []);
+
   if (premission === true) {
     return (
       <Container>
-        <Camera
-          style={{ width: WIDTH - 30, height: HEIGHT / 1.5 }}
-          type={cameraSetting.side}
-        />
+        <PinchGestureHandler onGestureEvent={handleZoomGesture}>
+          <Animated.View>
+            <Camera
+              style={{ width: WIDTH - 30, height: HEIGHT / 1.5 }}
+              type={cameraSetting.side}
+              zoom={JSON.stringify(cameraSetting.zoom)}
+            />
+          </Animated.View>
+        </PinchGestureHandler>
         <IconContainer>
           <TouchableOpacity onPress={handleCameraSideChangeBtnClick}>
             <Ionicons name={cameraSetting.icon} size={50} color="black" />
