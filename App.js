fc856459c2dd184fcd300590b817d8d5202ddf47
user <60804817+ichg4354@users.noreply.github.com>
2021-02-04 14:49:41 +0900

    add take photo when smile logic

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
 import {
   ActivityIndicator,
   Animated,
@@ -12,7 +12,8 @@ import {
 import { Camera } from "expo-camera";
 import { Ionicons } from "@expo/vector-icons";
 import styled from "styled-components";
-import { PinchGestureHandler } from "react-native-gesture-handler";
+import { LinearGradient } from "expo-linear-gradient";
+import * as FaceDetector from "expo-face-detector";
 
 const Container = styled.View`
   flex: 1;
@@ -24,7 +25,7 @@ const Container = styled.View`
 const IconContainer = styled.View`
   flex-direction: row;
   justify-content: center;
-  margin-top: 10px;
+  margin-top: 30px;
 `;
 
 export default function App() {
@@ -33,7 +34,9 @@ export default function App() {
   const [cameraSetting, setCameraSetting] = useState({
     side: "back",
     icon: "camera-reverse",
+    smile: false,
   });
+  const camera = useRef();
 
   const getPremission = async () => {
     try {
@@ -47,9 +50,35 @@ export default function App() {
 
   const handleCameraSideChangeBtnClick = () => {
     if (cameraSetting.side === "back") {
-      setCameraSetting({ side: "front", icon: "camera-reverse-outline" });
+      setCameraSetting({
+        side: "front",
+        icon: "camera-reverse-outline",
+        smile: false,
+      });
     } else {
-      setCameraSetting({ side: "back", icon: "camera-reverse" });
+      setCameraSetting({
+        side: "back",
+        icon: "camera-reverse",
+        smile: false,
+      });
+    }
+  };
+
+  const takePicture = async () => {
+    let photo = await camera.current.takePictureAsync();
+    console.log(photo);
+  };
+
+  const handleFaceDetection = (face) => {
+    const smilingProbability = face?.faces[0]?.smilingProbability;
+    if (smilingProbability >= 0.7) {
+      setCameraSetting({
+        smile: true,
+        side: cameraSetting.side,
+        icon: cameraSetting.icon,
+      });
+      console.log("SMILEing");
+      takePicture();
     }
   };
 
@@ -60,15 +89,42 @@ export default function App() {
   if (premission === true) {
     return (
       <Container>
-            <Camera
-              style={{ width: WIDTH - 30, height: HEIGHT / 1.5 }}
-              type={cameraSetting.side}
-              zoom={cameraSetting.zoom}
-            />
-          <TouchableOpacity onPress={handleCameraSideChangeBtnClick}>
-            <Ionicons name={cameraSetting.icon} size={50} color="black" />
-          </TouchableOpacity>
-        </IconContainer>
+        <LinearGradient
+          colors={["#4c669f", "#3b5998", "#192f6a"]}
+          style={{
+            width: "100%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Camera
+            style={{
+              width: WIDTH - 30,
+              height: HEIGHT / 1.5,
+              borderRadius: 30,
+              overflow: "hidden",
+            }}
+            type={cameraSetting.side}
+            onFacesDetected={
+              cameraSetting.smile ? null : (e) => handleFaceDetection(e)
+            }
+            faceDetectorSettings={{
+              mode: FaceDetector.Constants.Mode.fast,
+              detectLandmarks: FaceDetector.Constants.Landmarks.none,
+              runClassifications: FaceDetector.Constants.Classifications.all,
+              minDetectionInterval: 100,
+              tracking: true,
+            }}
+            onMountError={(e) => console.log(e)}
+            ref={camera}
+          />
+          <IconContainer>
+            <TouchableOpacity onPress={handleCameraSideChangeBtnClick}>
+              <Ionicons name={cameraSetting.icon} size={60} color="black" />
+            </TouchableOpacity>
+          </IconContainer>
+        </LinearGradient>
       </Container>
     );
   } else if (premission === false) {
